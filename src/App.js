82bafb5cf73bb6919f67ad9144c149469ac5a5bd
Vanshika Sharma <vanshika11082004@gmail.com>
2026-02-10 14:49:56 +0530

    understand and implement the useState Hook concept

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
 import Button from "./components/Button";
-import "./style.css"
+import "./style.css";
+import CounterComponent from "./components/counterComponent";
 const App =()=>{
   return(
     <div className ="todo-container">
+      <CounterComponent/>
       <Header text = "Todoie App"/>
       <TodoItem text = "Eat"/>
       <TodoItem text = "Code"/>
